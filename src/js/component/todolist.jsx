ab78ab9b0a8c5c02ce6032e8e4172b0e7bfeec34
Archiekins <ferreiranelson17295@gmail.com>
2022-08-03 17:39:22 +0200

    Final Todo List Fetch

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -2,31 +2,82 @@ import React, { useEffect, useState } from "react";
 
 import Inputs from "./input.jsx"
 
+const API_URL = "https://assets.breatheco.de/apis/fake/todos/user/Alex1kenobi";
+
 //create your first component
+
+
+async function fetchData() {
+    const response = await fetch(API_URL).catch(() => false);
+    console.log(response)
+    return response ? response.json() : [];
+}
+
+const udpateFetch = (todos) =>
+    fetch(API_URL, {
+        method: "PUT",
+        body: JSON.stringify(todos),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).catch((err) => {
+        console.log(err);
+        return false;
+    });
+
+const updateData = async (todos) => {
+    const resp = await udpateFetch(todos);
+
+    if (!resp) {
+        alert("Error updating data");
+        return resp;
+    }
+
+    const data = await resp.json();
+    alert(data.result);
+    return true;
+};
+
+
 const TodoList = () => {
+
     const [value, setValue] = useState("");
-    const [list, setList] = useState([]);
+    const [list, setList] = useState([() => fetchData()]);
     const [key, setKey] = useState(0);
 
+
+    
     const handleValueChange = (e) => setValue(e.target.value);
 
     const onSubmit = (e) => {
         e.preventDefault();
-        const newList = [...list, { label: value, done: false, key: key }];
+        const newList = [...list, { label: value, done: false}];
         setKey(key + 1)
         setList(newList);
         setValue("");
+        updateData(newList);
+
     };
 
     const handleDone = (key) => {
-        const newList = list.filter((item) => item.key !== key);
+        const newList = list.filter((item) => item.label !== key);
         const newListDone = list.map((listItem) => {
-            if (listItem.key === key)
-                return { key: listItem.key, label: listItem.label, done: true, };
+            if (listItem.label === key)
+                return {  label: listItem.label, done: true, };
             return listItem;
         });
+        
         setList(newList);
+        updateData(newList);
     };
+
+
+    useEffect(() => {
+        // Get all the data from the API  --> Pass it to my local state so I can play with it
+        fetchData().then((data) => setList(data));
+    }, []);
+    // The initlization of our local state
+
 
     return (
         <>
@@ -35,11 +86,11 @@ const TodoList = () => {
             </form>
             {list.map((item) => (
                 <div className="todoList"
-                    key={item.key}
-                    style={{display: "flex", alignItems: "center" }}
+                    key={item.label}
+                    style={{ display: "flex", alignItems: "center" }}
                 >
                     <h5>{item.label}</h5>
-                    <button onClick={() => handleDone(item.key)}> x </button>
+                    <button onClick={() => handleDone(item.label)}> x </button>
                 </div>))}
         </>
     );
